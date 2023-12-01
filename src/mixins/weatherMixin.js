61a0acd6880ed6a1f87fc55bfce493cc0ffac0da
woo seong won <133616084+wdevelope@users.noreply.github.com>
2023-12-01 12:23:08 +0900

    Update weatherMixin.js

--- src/mixins/weatherMixin.js
+++ src/mixins/weatherMixin.js
@@ -7,7 +7,6 @@ const weatherMixin = {
   methods: {
     async getWeatherInfo(city) {
       const API_KEY = process.env.VUE_APP_API_KEY;
-      console.log(API_KEY);
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.code}&appid=${API_KEY}`;
       const res = await axios.get(url);
       const { main, wind, weather } = res.data;
